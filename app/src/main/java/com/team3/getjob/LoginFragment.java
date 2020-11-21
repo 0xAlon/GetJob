b6b54962ac1490f61777ecea9a6cb77b94c30b25
Alon <alon.ttp@gmail.com>
2020-11-21 11:13:20 +0200

    Login page design
    Update login page design

--- app/src/main/java/com/team3/getjob/LoginFragment.java
+++ app/src/main/java/com/team3/getjob/LoginFragment.java
@@ -22,8 +22,7 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_login, container, false);
-        return view;
+        return inflater.inflate(R.layout.fragment_login, container, false);
     }
 
     public LoginFragment(FragmentManager fragmentManager) {
