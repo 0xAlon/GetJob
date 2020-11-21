1407340af1046ba04586348de8819a07b9483200
Michael <50995165+MikeShveyn@users.noreply.github.com>
2020-11-21 22:40:28 +0200

    Add another activity

---
+++ app/src/main/java/com/team3/getjob/Jobs_Pull.java
@@ -0,0 +1,14 @@
+package com.team3.getjob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Jobs_Pull extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jobs__pull);
+    }
+}
