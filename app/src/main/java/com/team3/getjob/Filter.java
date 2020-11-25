3686fc6b5f670815039a0540ece629234d1e002c
Anna Rabynovych <rabynan@ac.sce.ac.il>
2020-11-25 12:14:43 +0200

    begin to work on design

---
+++ app/src/main/java/com/team3/getjob/Filter.java
@@ -0,0 +1,14 @@
+package com.team3.getjob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Filter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+    }
+}
