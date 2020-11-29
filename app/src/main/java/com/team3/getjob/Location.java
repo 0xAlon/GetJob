f451f45805a8c420793c1290b237c1ebf35b6b66
Anna Rabynovych <rabynan@ac.sce.ac.il>
2020-11-29 15:39:02 +0200

    design filter activity

--- app/src/main/java/com/team3/getjob/Location.java
+++ app/src/main/java/com/team3/getjob/Location.java
@@ -2,7 +2,10 @@ package com.team3.getjob;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Location extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class Location extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Location.this, Filter.class);
+                startActivity(intent);
+            }
+        });
     }
 }
