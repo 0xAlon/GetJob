c6808cebe3f7febbc179757bd00506185847aff9
Anna Rabynovych <rabynan@ac.sce.ac.il>
2020-11-29 15:09:34 +0200

    Working on Location Filter

--- app/src/main/java/com/team3/getjob/Filter.java
+++ app/src/main/java/com/team3/getjob/Filter.java
@@ -2,7 +2,10 @@ package com.team3.getjob;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Filter extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class Filter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+        Button button = (Button) findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Filter.this, Location.class);
+                startActivity(intent);
+            }
+        });
     }
 }
