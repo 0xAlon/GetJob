f8918dcaa4a940e340050e00a02f70f8eee44b80
Artiom Bodilovski <73061550+ArtBod@users.noreply.github.com>
2020-11-30 12:56:02 +0200

    upload employer profile
    the profile upload done

--- app/src/main/java/com/team3/getjob/EmployerProfile.java
+++ app/src/main/java/com/team3/getjob/EmployerProfile.java
@@ -1,7 +1,6 @@
 package com.team3.getjob;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.FirebaseApp;
@@ -11,51 +10,69 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class EmployerProfile extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private String TAG="EmployerProfile";
+
     TextView user_name;
     TextView email;
     TextView phone_num;
     TextView company_name;
     TextView loction;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.employer_profile);
+
         FirebaseApp.initializeApp(this);
         mAuth = FirebaseAuth.getInstance();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        user_name = (TextView)findViewById(R.id.user_name);
-        //user_name.setText(SetUserInfo("Name").get(0));
-        SetUserInfo("Name").get(0);
+        user_name = (TextView) findViewById(R.id.user_name);
+        email = (TextView) findViewById(R.id.email);
+        phone_num = (TextView) findViewById(R.id.phone);
+        company_name = (TextView) findViewById(R.id.company_name);
+        loction = (TextView) findViewById(R.id.location);
+
     }
 
-    ArrayList<String> SetUserInfo(String value)
-    {
-        mAuth = FirebaseAuth.getInstance();
+    @Override
+    public void onStart() {
+        super.onStart();
         FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateFields(currentUser);
+    }
+
+    private void updateFields(FirebaseUser currentUser){
+
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        ArrayList<String> temp_list = new ArrayList<String>(1);
+
         db.collection("Users").whereEqualTo("Uid", "W4CyywCheWVdQfpTLocUC8Kfysj1")
                 .get()
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
-
+                        List<String> list = new ArrayList<>();
                         for (QueryDocumentSnapshot document : task.getResult()) {
                             Log.d(TAG, document.getId());
-                            temp_list.add(document.getString(value));
+                            list.add(String.valueOf(document.getData().get("Name")));
+                            list.add(String.valueOf(document.getData().get("Email")));
+                            list.add(String.valueOf(document.getData().get("PhoneNumber")));
+                            list.add(String.valueOf(document.getData().get("Company ")));
+                            list.add(String.valueOf(document.getData().get("Address ")));
                         }
+                        user_name.setText(String.valueOf(list.get(0)));
+                        email.setText(String.valueOf(list.get(1)));
+                        phone_num.setText(String.valueOf(list.get(2)));
+                        company_name.setText(String.valueOf(list.get(3)));
+                        loction.setText(String.valueOf(list.get(4)));
+
                     } else {
                         Log.d(TAG, "Error getting documents: ", task.getException());
                     }
                 });
-        return temp_list;
     }
-
 
 }
