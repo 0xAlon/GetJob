04abf7f57232a93c9bd47c7a1b464e67d1c2406e
Alon <alon.ttp@gmail.com>
2020-12-16 18:25:32 +0200

    Add Firebase functionality
    Setup the ability to show user name in the navigation drawer

--- app/src/main/java/com/team3/getjob/BaseActivity.java
+++ app/src/main/java/com/team3/getjob/BaseActivity.java
@@ -1,7 +1,11 @@
 package com.team3.getjob;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
@@ -9,6 +13,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 public class BaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     protected FrameLayout frameLayout;
@@ -16,6 +25,10 @@ public class BaseActivity extends AppCompatActivity implements NavigationView.On
 
     private DrawerLayout drawer;
     private int INTENT_CAMERA_CODE = 100;
+    private String TAG = "BaseActivity";
+    private FirebaseAuth mAuth;
+    MenuItem name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,10 +36,44 @@ public class BaseActivity extends AppCompatActivity implements NavigationView.On
         //context=this;
         frameLayout = (FrameLayout) findViewById(R.id.container);
 
-        //DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
 
         navigationView.setNavigationItemSelectedListener(this);
+
+        Menu menu = navigationView.getMenu();
+        name = menu.findItem(R.id.name);
+
+        initUserName();
+    }
+
+    private void initUserName() {
+
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        if (currentUser != null){
+            db.collection("Users").whereEqualTo("Uid", currentUser.getUid())
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                if (currentUser.getUid().equals(String.valueOf(document.getData().get("Uid")))) {
+                                    name.setTitle(String.valueOf(document.getData().get("Name")));
+                                }
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    });
+
+        }
+        else{
+            name.setTitle("Error getting Name");
+        }
     }
 
     @Override
@@ -41,7 +88,10 @@ public class BaseActivity extends AppCompatActivity implements NavigationView.On
         } else if (id == R.id.about) {
 
         } else if (id == R.id.git){
-
+            String url = "https://github.com/0xAlon/GetJob";
+            Intent browserIntent =
+                    new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(browserIntent);
         }
             //drawer.closeDrawer(GravityCompat.START);
         return true;
