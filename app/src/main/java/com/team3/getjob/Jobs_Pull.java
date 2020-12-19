f934376a88c1e8600d0b3e40b72ba9d43d674004
Michael <50995165+MikeShveyn@users.noreply.github.com>
2020-12-19 18:00:55 +0200

    Setup Pull and filter
    add:
    Information to intent from filter to jobsPull
    apply changes in data base call

--- app/src/main/java/com/team3/getjob/Jobs_Pull.java
+++ app/src/main/java/com/team3/getjob/Jobs_Pull.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -45,9 +48,11 @@ public class Jobs_Pull extends AppCompatActivity {
         mListView = findViewById(R.id.list_view);
         db = FirebaseFirestore.getInstance();
 
+
         List<String> id_list = new ArrayList<String>();
         ArrayList<job_model> temp_list = new ArrayList<job_model>();
         Context context = this;
+
 
         db.collection("Posts")
                 .get()
@@ -74,6 +79,15 @@ public class Jobs_Pull extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 jobs_pull_fragment jobsPullFragment = jobs_pull_fragment.newInstance(id_list.get(i));
                 getSupportFragmentManager().beginTransaction().addToBackStack(null).add(R.id.jobs_pull_frame, jobsPullFragment).commit();
+            }
+        });
+
+        SearchView filter_button = (SearchView) findViewById(R.id.jobs_search);
+        filter_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(Jobs_Pull.this, Filter.class);
+                startActivity(intent2);
             }
         });
     }
